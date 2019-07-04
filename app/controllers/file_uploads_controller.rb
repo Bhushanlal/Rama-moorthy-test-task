34fba871dca0ef516e95ebee66e233261dad9ddd
BhushanLal <bhushanlal972@gmail.com>
2019-07-04 16:59:46 +0530

    file conversion done

--- app/controllers/file_uploads_controller.rb
+++ app/controllers/file_uploads_controller.rb
@@ -8,8 +8,18 @@ class FileUploadsController < ApplicationController
 
   #action for saving the data from file 
   def file_content
+    require "base64"
+      content = params[:file]
+      decode_base64_content = Base64.decode64(content) 
+      File.open(Rails.root.join('public', "Output.xlsx"), "wb") do |f|
+        f.write(decode_base64_content)
+      end 
+
+    file_path = Rails.root.join('public') + "Output.xlsx"
+
     begin
-      uploaded_file = Roo::Spreadsheet.open(params[:file].path, extension: :xlsx)
+      #uploaded_file = Roo::Spreadsheet.open(params[:file].path, extension: :xlsx)
+      uploaded_file = Roo::Spreadsheet.open(file_path, extension: :xlsx)
       file_header = uploaded_file.row(1)
       (2..uploaded_file.last_row).each do |i|
         row = Hash[[file_header, uploaded_file.row(i)].transpose]
